98d632c23f044a34374b3bef964a43fbb5cfb10e
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-11-12 22:14:50 +1100

    chore: add theme globally in App component to customize default themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const materialTheme = createTheme({
+  palette: {
+    secondary: {
+      main: '#fefefe'
+    }
+  }
+})
 
 function App() {
 
   return (
-    <BrowserRouter future={{ v7_startTransition: true }}>
-      <Routes>
-        <Route path="/" element={<Create />} />
-        <Route path="/Notes" element={<Notes />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={materialTheme}>
+      <BrowserRouter future={{ v7_startTransition: true }}>
+        <Routes>
+          <Route path="/" element={<Create />} />
+          <Route path="/Notes" element={<Notes />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
